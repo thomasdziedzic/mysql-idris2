d433159821a61cea614ba361e6a82e595e529310
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2020-08-06 22:10:57 -0500

    get rid of example code

--- libidrismysql/libidrismysql.c
+++ libidrismysql/libidrismysql.c
@@ -1,23 +1,6 @@
 #include <mysql/mysql.h>
 
-/*
-MYSQL_ROW row;
-unsigned int num_fields;
-unsigned int i;
-
-num_fields = mysql_num_fields(result);
-while ((row = mysql_fetch_row(result)))
-{
-   unsigned long *lengths;
-   lengths = mysql_fetch_lengths(result);
-   for(i = 0; i < num_fields; i++)
-   {
-       printf("[%.*s] ", (int) lengths[i],
-              row[i] ? row[i] : "NULL");
-   }
-   printf("\n");
-}
-*/
+#include "libidrismysql.h"
 
 char *get_column(MYSQL_ROW row, unsigned int index) {
     return row[index];
